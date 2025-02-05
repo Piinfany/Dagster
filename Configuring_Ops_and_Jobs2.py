942a795f20949ba480218a9cd3b26e30dff66d1c
Piinfany <jirada.dec@gmail.com>
2025-02-05 17:22:15 +0700

    commit

---
+++ Configuring_Ops_and_Jobs2.py
@@ -0,0 +1,3 @@
+# Write a job that uses a configuration file to decide which op to run.
+# สร้าง Dagster Job ที่ใช้ไฟล์คอนฟิกูเรชันเพื่อกำหนดว่าให้รัน Operation (หรือ op) ตัวไหน โดยใช้ภาษา Python ในการพัฒนา
+
---
+++ Configuring_Ops_and_Jobs3.py
@@ -0,0 +1,2 @@
+# Build a pipeline that accepts user input via configuration for a greeting message.
+
